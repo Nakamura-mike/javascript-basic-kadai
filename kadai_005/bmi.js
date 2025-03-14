b785abc302f630e0358deef64399bf63843bfce7
Nakamura-mike <161773906+Nakamura-mike@users.noreply.github.com>
2025-03-14 11:30:01 +0900

    ディレクトリ「kadai_005」をプッシュ

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+// 変数の宣言・値の代入
+let personWeight = 68;
+let personHeight = 1.7;
+let personBMI = 0;
+
+// 値の代入
+personBMI = personWeight / (personHeight * personHeight);
+
+// コンソールへの出力
+console.log(personBMI);
